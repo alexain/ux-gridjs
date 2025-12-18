1cca53d4aa96e183c70b670675916db91f3ed0c3
alexain <xainbit@gmail.com>
2025-12-18 18:04:47 +0100

    fix load services in UxGridjsBundle.php

--- src/UxGridjsBundle.php
+++ src/UxGridjsBundle.php
@@ -8,17 +8,22 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 final class UxGridjsBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        // Registra i servizi del bundle (GridFactory, Twig extension, ecc.)
+        $container->import(__DIR__ . '/Resources/config/services.yaml');
+    }
+
     public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         if (!class_exists(\Symfony\Component\AssetMapper\AssetMapper::class)) {
             return;
         }
 
-
         $builder->prependExtensionConfig('framework', [
             'asset_mapper' => [
                 'paths' => [
-                    __DIR__.'/../assets' => '@alexain/ux-gridjs',
+                    __DIR__ . '/../assets' => '@alexain/ux-gridjs',
                 ],
             ],
         ]);

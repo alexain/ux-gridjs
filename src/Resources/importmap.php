a973a355395e482078aa015ade1bc8fd5e799b8a
alexain <xainbit@gmail.com>
2025-12-18 16:35:35 +0100

    change grid_controller.js path

---
+++ src/Resources/importmap.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    // dipendenza JS principale
+    'gridjs' => [
+        'version' => '6.2.0'
+    ],
+    'gridjs/dist/theme/mermaid.min.css' => [
+        'version' => '6.2.0',
+        'type' => 'css',
+    ],
+    'gridjs/l10n' => [
+        'version' => '6.2.0',
+    ],
+];

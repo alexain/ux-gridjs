6b11bcd3da1d4562ab95221c35aa907061b6019d
alexain <xainbit@gmail.com>
2025-12-18 16:58:13 +0100

    change grid_controller.js path again

--- src/UxGridjsBundle.php
+++ src/UxGridjsBundle.php
@@ -2,8 +2,25 @@
 
 namespace Alexain\UxGridjs;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-final class UxGridjsBundle extends Bundle
+final class UxGridjsBundle extends AbstractBundle
 {
+    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        if (!class_exists(\Symfony\Component\AssetMapper\AssetMapper::class)) {
+            return;
+        }
+
+
+        $builder->prependExtensionConfig('framework', [
+            'asset_mapper' => [
+                'paths' => [
+                    __DIR__.'/../assets' => '@alexain/ux-gridjs',
+                ],
+            ],
+        ]);
+    }
 }
